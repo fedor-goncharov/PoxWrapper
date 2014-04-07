14badf99037cb0975ec51eb013ad5c89d9ffb1fb
fedor.goncharov <fedor.goncharov.ol@gmail.com>
2014-04-07 19:59:40 +0400

    First commit, project base

---
+++ src/ru/mail/fedka2005/objects/ControllerWrapper.java
@@ -0,0 +1,68 @@
+package ru.mail.fedka2005.objects;
+import java.net.InetAddress;
+import org.jgroups.JChannel;
+/**
+ * Class represents a domain in the cluster of controllers
+ * A group of ControllerWrapper instances gives a cluster.
+ * Target - synchronize a number of pox-controllers, choose
+ * the master, and return address of the master controller.
+ * Another task, when controller is chosen, one must monitor it's
+ * state.
+ * @author fedor
+ *
+ */
+
+public class ControllerWrapper implements Runnable {
+	public ControllerWrapper(String groupName, String address, String pName,
+			String poxPath, int poxPort) 
+			throws Exception {
+		try {
+			this.groupName = groupName;
+			this.groupAddress = InetAddress.getByName(address);
+			this.pName = pName;
+			this.poxPath = poxPath;
+			this.poxPort = poxPort;
+		} catch (Exception e) {
+			throw new Exception("ControllerWrapper constructor");
+		}
+	}
+	
+	public void Start() throws Exception {
+		
+		try {
+			channel = new JChannel();
+			//TODO
+			//add url props when initializong JChannel
+			channel.setName(pName);
+			channel.connect(groupName);		//handle exception
+		} catch (Exception e) {
+			throw new Exception("ControllerWrapper.Start(), message:" + e.toString());
+		} finally {
+			try {
+				if (channel != null) {channel.close();}
+			} catch (Exception e) {};
+		}
+		//TODO
+		//add listener
+		
+	}
+	
+	private JChannel channel = null;
+	//private UUID uuid;	//unique identifier of the node
+	private InetAddress groupAddress;
+	private String groupName;
+	private String pName;
+	private boolean active = false;
+	private int poxPort;
+	private String poxPath;
+	
+	@Override
+	public void run() {
+		try {
+			this.Start();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			System.exit(1);
+		}
+	}
+}
---
+++ src/ru/mail/fedka2005/tests/ControllerWrapperTest.java
@@ -0,0 +1,11 @@
+package ru.mail.fedka2005.tests;
+
+import org.junit.Test;
+
+public class ControllerWrapperTest {
+	@Test public void testControllerWrapper() {
+		//TODO
+		//add some tests here
+	}
+
+}
