9a987a140d72ee473519c9514505af3df22d996d
fedor.goncharov <fedor.goncharov.ol@gmail.com>
2014-04-09 20:14:38 +0400

    minor changes for no-master client

--- src/ru/mail/fedka2005/messages/RecvMessageHandler.java
+++ src/ru/mail/fedka2005/messages/RecvMessageHandler.java
@@ -8,7 +8,7 @@ public class RecvMessageHandler {
 	public static final int CPU_REQUEST = 101;
 	public static final int UNKNOWN = 0;
 	
-	public int getMessageType(Message msg) {
+	public static int getMessageType(Message msg) {
 		Object obj = msg.getObject();
 		if (obj instanceof CPULoadMessage) {
 			return CPU_NOTIFICATION;
@@ -18,7 +18,7 @@ public class RecvMessageHandler {
 		}
 		return UNKNOWN;
 	}
-	public CPULoadRecord getCPULoad(Message msg) throws ClassCastException {
+	public static CPULoadRecord getCPULoad(Message msg) throws ClassCastException {
 		return new CPULoadRecord(msg.getSrc(),((CPULoadMessage)msg.getObject()).cpuLoad);
 	}
 }
--- src/ru/mail/fedka2005/objects/ControllerWrapper.java
+++ src/ru/mail/fedka2005/objects/ControllerWrapper.java
@@ -1,4 +1,6 @@
 package ru.mail.fedka2005.objects;
+import java.util.EmptyStackException;
+import java.util.Stack;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 
@@ -25,6 +27,8 @@ import ru.mail.fedka2005.messages.*;
 
 //TODO
 //add log4j for logging all events
+//replace sleeping with time-scheduler
+//generate mapping between addresses and id's
 
 public class ControllerWrapper implements Runnable {
 	/**
@@ -41,12 +45,14 @@ public class ControllerWrapper implements Runnable {
 	 * @throws Exception
 	 */
 	public ControllerWrapper(String groupName, String groupAddress, String pName, long id,
-			String poxPath, int poxPort) throws Exception {
+			String poxPath, int poxPort, double cpuThreshold) throws Exception {
 		try {
 			this.groupName = groupName;
 			this.groupAddress = groupAddress;
 			this.pName = pName; this.id = id;
 			this.poxPath = poxPath; this.poxPort = poxPort;
+			this.mNotifications = new Stack<CPULoadRecord>();
+			ControllerWrapper.cpuThreshold = cpuThreshold;
 		} catch (Exception e) {
 			throw new Exception("ControllerWrapper constructor");
 		}
@@ -61,15 +67,17 @@ public class ControllerWrapper implements Runnable {
 			channel.setName(pName);
 			id_service = new CounterService(channel);
 			channel.connect(groupName); isActive = true;
+			LockService lock_service = new LockService(channel);
+			Lock masterLock = lock_service.getLock("change_master_lock");
 
 			channel.setReceiver(new ReceiverAdapter() {
 				public void receive(Message msg) {
 					//TODO - logging
-					switch (RecvMessageHandler.instance.getMessageType(msg)) {
+					switch (RecvMessageHandler.getMessageType(msg)) {
 						case RecvMessageHandler.CPU_NOTIFICATION : {
-							//TODO
-							//write load to handling this message
-							//add message to stack
+							if (mNotifications.size() > 0) mNotifications.clear();
+							mNotifications.push(RecvMessageHandler.getCPULoad(msg));
+							break;
 						}
 						case RecvMessageHandler.CPU_REQUEST :  {
 							//TODO
@@ -79,6 +87,9 @@ public class ControllerWrapper implements Runnable {
 						case RecvMessageHandler.UNKNOWN : {
 							//TODO
 							//throw Exception
+						}
+						default : {
+							//throw unknow type of message
 						}
 					}
 					
@@ -98,7 +109,7 @@ public class ControllerWrapper implements Runnable {
 						//check if it was master and replace if required
 				}
 			});
-			eventLoop(channel, id_service.getOrCreateCounter("master", id));
+			eventLoop(channel, id_service.getOrCreateCounter("master", id), masterLock);
 		} catch (Exception e) {
 			throw new Exception("ControllerWrapper.start(), message:" + e.toString());
 		} finally {
@@ -107,14 +118,21 @@ public class ControllerWrapper implements Runnable {
 			} catch (Exception e) {};
 		}
 	}
-	private void eventLoop(JChannel channel, Counter masterID) throws Exception {
+	/**
+	 * Node main loop, monitoring messages, and switching master between nodes
+	 * appropirately.
+	 * @param channel
+	 * @param masterID
+	 * @param masterLock
+	 * @throws Exception
+	 */
+	private void eventLoop(JChannel channel, Counter masterID, Lock masterLock) 
+			throws Exception {
 		boolean rewrite = false;	//to switch: ovs-vsctl set-controller (me)
-		LockService lock_service = new LockService(channel);
-		Lock masterLock = lock_service.getLock("change_master_lock");
 		while (isActive) {
 			while (masterID.get() == id) {	//cpu-load notification for the cluster
 				if (!rewrite) {
-					masterLock.lock();
+					masterLock.lock();		//concurrent rewriting excluded
 						if (masterID.get() == id) {
 							//TODO
 							//for all ovs's
@@ -124,33 +142,77 @@ public class ControllerWrapper implements Runnable {
 					masterLock.unlock();
 				}
 				channel.send(new Message(null, new CPULoadMessage()));
-				TimeUnit.SECONDS.sleep(DELAY);
+				TimeUnit.SECONDS.sleep(SEND_DELAY);
 			}
 			rewrite = false;
+			boolean wait_stack = true;
 			while (masterID.get() != id) {
+				try {
+					CPULoadRecord record = mNotifications.pop();
+					if (record.getCPULoad() > cpuThreshold) {	//if cpu-load is too-high -> replace master
+						replaceMaster();
+						wait_stack = true;
+						continue;
+					}
+				} catch (EmptyStackException e) {
+					if (wait_stack) {
+						TimeUnit.SECONDS.sleep(RECV_DELAY);
+						wait_stack = false;
+						continue;
+					} else {
+						replaceMaster();
+						continue;
+					}
+				}
 				//TODO
 				//monitor the master, wait for incoming messages
 				//update timer, if time exceeds then - change master
 			}
-			
 		}
 	}
-	//private void replaceMaster() {};
+	/**
+	 * replaces the master controller, depends on the reason of replacement:
+	 * exeeded notification await time, suspected for crush, high cpu-load on the node.
+	 * @param masterID
+	 * @param masterLock
+	 * @param code
+	 */
+	private void replaceMaster(Counter masterID, Lock masterLock, int code) {
+		switch (code) {
+		case EXCEEDTIME :
+			break;
+		case CPU_LOAD : 
+			break;
+		case CRASH_SUSPECT : 
+			break;
+		default : //throw UnknowTypeofReplacement
+		}
+	}
+			
+	//static
+	private static final int EXCEEDTIME = 200;
+	private static final int CPU_LOAD = 201;
+	private static final int CRASH_SUSPECT = 203;
+	private static double cpuThreshold = 90;	//maximum cpu load for master
 	
-	//dynamic variables
+	//dynamic
 	private JChannel channel = null;
 	private View clView;						//will be required later
 	private CounterService id_service = null;
 	private long id;							//node id
+	private Stack<CPULoadRecord> mNotifications = null;
+	private LockService lock_service = null;
+	private Lock masterLock = null;
 	
-	//config, static variables
+	//config
 	private String groupAddress;
 	private String groupName;
 	private String pName;
 	private boolean isActive = false;
 	private int poxPort;
 	private String poxPath;
-	public static int DELAY = 2;	//delay in seconds between cpu-load notification
+	public static final int SEND_DELAY = 2;	//send delay in seconds between cpu-load notifications
+	public static final int RECV_DELAY = 3; //recieve delay in seconds between cpu-load notifications
 	
 	//TODO
 	//add getters and setters, later
